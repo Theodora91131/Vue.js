0b817f97d7508403d167448e3716d642585412cb
unknown <10556003@ntub.edu.tw>
2023-01-20 00:07:32 +0800

    variantImage

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ const app = Vue.createApp({
             inventory: 1,
             details:['50% cotton','30% wool','20% polyester'],
             variants:[
-                {id: 2234, color:'green'},
-                {id: 2235, color:'blue'}
+                {id: 2234, color:'green',image: './assets/images/socks_green.jpg'},
+                {id: 2235, color:'blue',image: './assets/images/socks_blue.jpg'}
 
             ]
         }
@@ -17,8 +17,16 @@ const app = Vue.createApp({
     methods: {
         addToCart(){
             this.cart += 1
+        },
+        updateImage(variantImage){
+            this.image = variantImage
         }
     }
+    
+
+
+
+    
 
 
 })
