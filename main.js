69fd9e479c7b51e3f5c2d883fe31290201b006c8
unknown <10556003@ntub.edu.tw>
2023-01-19 22:26:02 +0800

    v-for

--- main.js
+++ main.js
@@ -3,7 +3,13 @@ const app = Vue.createApp({
         return{
             product: 'Socks',
             image: './assets/images/socks_green.jpg',
-            inventory: 1
+            inventory: 1,
+            details:['50% cotton','30% wool','20% polyester'],
+            variants:[
+                {id: 2234, color:'green'},
+                {id: 2235, color:'blue'}
+
+            ]
         }
 
     }
