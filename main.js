d9c3ac5d27627bd059a744406404f1dce42e855a
unknown <10556003@ntub.edu.tw>
2023-01-21 08:27:52 +0800

    calculate

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const app = Vue.createApp({
         return{
             cart: 0,
             product: 'Socks',
+            brand:'Vue Mastery',
             image: './assets/images/socks_green.jpg',
             inStock: false,
             inventory: 1,
