c6376d1e1e0c8ee7b95eea60cd915019e0ee5385
unknown <10556003@ntub.edu.tw>
2023-01-24 11:12:00 +0800

    add-review

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -41,7 +41,9 @@ app.component('product-display',{
             </button>
           </div>
         </div>
-        <review-form></review-form>
+       </div>
+       <review-list v-if="reviews.length" :reviews="reviews"></review-list>
+        <review-form @review-submitted="addReview"></review-form>
       </div>`,
       data(){
         return{
@@ -54,7 +56,8 @@ app.component('product-display',{
                 {id: 2234, color:'green',image: './assets/images/socks_green.jpg', quantity:50},
                 {id: 2235, color:'blue',image: './assets/images/socks_blue.jpg', quantity:0},
 
-            ]
+            ],
+            reviews:[]
         }
 
     },
@@ -65,6 +68,9 @@ app.component('product-display',{
         updateVariant(index){
             this.selectedVariant = index
     
+        },
+        addReview(review){
+            this.reviews.push(review)
         }
     },
     computed: {
