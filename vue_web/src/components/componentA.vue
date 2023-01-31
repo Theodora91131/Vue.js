c96ce960b77fd53b5907f23f981b104733a14806
Theodora91131 <10556003@ntub.edu.tw>
2023-02-01 02:10:50 +0800

    docs:data two way binding

--- vue_web/src/components/componentA.vue
+++ vue_web/src/components/componentA.vue
@@ -1,11 +1,16 @@
 <script setup>
+import {ref} from "@vue/reactivity"
 const data = "Hello Vue"
+const text = ref("123")
 </script>
 
 <template>
  <p>Hello World</p>
  <!--data one way binding-->
    {{ data }}
+<!--data two way binding-->
+<input type = "text" v-model="text"/> 
+<p>{{ text }}</p>
 </template>
 
 <style >
