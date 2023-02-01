cb46d68a64c7ead7e9715d0e092fa3475582df56
Theodora91131 <10556003@ntub.edu.tw>
2023-02-01 20:21:50 +0800

    feat:button show black screen

--- vue_web/src/components/componentA.vue
+++ vue_web/src/components/componentA.vue
@@ -3,17 +3,16 @@ import {ref} from "@vue/reactivity"
 const data = "Hello Vue"
 const text = ref("123")
 const view = ref(1)
-const changeView=()=>{
-    view.value = 2
+const changeView=(index)=>{
+    view.value = index
 }
-const changeView2=()=>{
-    view.value = 1
-}
+
 </script>
 
 <template>
-<button v-on:click="changeView">show one-way binding</button>
-<button v-on:click="changeView2">show two-way binding</button>
+<button v-on:click="changeView(1)">show one-way binding</button>
+<button @click="changeView(2)">show two-way binding</button>
+<button @click="changeView(100)">show black screen </button>
 <div class ="container-1" v-if="view==1">
  <p>Hello World</p>
  <!--data one-way binding-->
@@ -24,6 +23,7 @@ const changeView2=()=>{
 <input type = "text" v-model="text"/> 
 <p>{{ text }}</p>
 </div>
+<div v-else> black screen </div>
 </template>
 
 <style >
