4c72a3f811ea9912e7abb6010a8162c1136b7510
Theodora91131 <10556003@ntub.edu.tw>
2023-02-03 12:39:06 +0800

    feat:animation

--- vue_web_for/src/App.vue
+++ vue_web_for/src/App.vue
@@ -1,17 +1,23 @@
 <script setup>
-const dataList = [
-  'a',
-  'b',
-  'c',
-  'd',
-  '123',
-  'asd'
-]
+import {ref} from 'vue';
+const test = ref('test')
+const dataList = {
+  key1:'a',
+  key2:'b',
+  key3:'c',
+  key4:'d'
+}
 </script>
 
 <template>
-  <p v-for="text in dataList">
-    {{ text }}
+  <p class="test-cls">{{ test }}</p>
+  <p v-for="(value,key,index) in dataList"
+  :key="value">
+    <span class="hint" v-show="key !=='key3'">
+      {{ index }}.{{ key }}:{{ value }}
+
+    </span>
+   
   </p>
   
 </template>
@@ -28,4 +34,23 @@ const dataList = [
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
+.test-cls{
+  font-size:40px;
+  animation:openln 1s ease-in-out;
+}
+@keyframes openln{
+  0%{
+    opacity:0;
+    transform:translateY(20px);
+  }
+  100%{
+    opacity:1;
+    transform:translateY(0);
+  }
+
+
+}
+
+
+
 </style>
