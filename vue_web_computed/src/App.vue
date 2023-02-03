ea04773001468ea9f048a467d12e429e78db6560
Theodora91131 <10556003@ntub.edu.tw>
2023-02-03 17:04:59 +0800

    feat:computed

---
+++ vue_web_computed/src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+const num = 0.6
+</script>
+
+<template>
+  <h1>Computed計算屬性</h1>
+  <h3>{{Number.isNaN(num*100)? num : num*100 }}%</h3>
+
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
