58e9f37c982ffc80721c91d97afc64dd9e404acf
Theodora91131 <10556003@ntub.edu.tw>
2023-02-13 19:22:07 +0800

    feat:Props

---
+++ vue_components/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <input type=" text" v-model="msg"/>
+  <br/>
+  <Child v-bind:data="msg"/>
+</template>
+
+
+<script setup>
+import {ref} from 'vue'
+import Child from './components/Child.vue'
+const msg = ref('')
+</script>
+
+
+
+<style >
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ vue_components/src/components/Child.vue
@@ -0,0 +1,19 @@
+<template>
+    <!--div>
+      字數:{{msgCount}}
+    </div-->
+   data: {{ data }}
+</template>
+<script setup>
+import { computed } from '@vue/reactivity'
+defineProps(['data'])
+//const msgCount = computed(()=>{
+//    return XXX.length
+//})
+</script>
+
+
+
+<style scoped lang="postcss">
+
+</style>
